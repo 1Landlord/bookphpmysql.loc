7670a66cf121fcf8ee2b3ce4ec175d942c1aea70
1Landlord <vl_che@bk.ru>
2018-01-17 04:57:59 +0300

    Новый запрос на извлечение данных из базы и цикл

--- guitarwars/index.php
+++ guitarwars/index.php
@@ -23,22 +23,30 @@
     require_once('connectvars.php');
 
   // Извлечение данных рейтингов из базы MySQL
-  $query = "SELECT * FROM guitarwars";
+  //$query = "SELECT * FROM guitarwars";
+  //Новый запрос извлечения данных, в порядке убывания score (score DESC)
+  $query = "SELECT * FROM guitarwars ORDER BY score DESC, date ASC";
   $data = mysqli_query($dbc, $query);
 
   // Извлечение данных из массива в цикле. Формирование данных записей в виде кода HTML 
   echo '<table>';
+  $i = 0;
   while ($row = mysqli_fetch_array($data)) { 
     // Вывод данных рейтинга
+    if ($i == 0) {
+        echo '<tr><td colspan="2" class="topscoreheader">Top Score: ' . $row['score'] . '</td></tr>';
+      }
     echo '<tr><td class="scoreinfo">';
     echo '<span class="score">' . $row['score'] . '</span><br />';
     echo '<strong>Имя:</strong> ' . $row['name'] . '<br />';
     echo '<strong>Дата:</strong> ' . $row['date'] . '</td></tr>';
-    if (is_file(GW_UPLOADPACH . $row['screenshot']) &&filesize(GW_UPLOADPACH . $row['screenshot'])>o) {
-        echo '<td><img src="' . GW_UPLOADPACH . $row['screenshot'] .'" alt="Подтверждено"/></td></tr>';
-    }else {
-        echo '<td><img src="' . GW_UPLOADPACH . 'unverified.gif' .'" alt="Не подтверждено!"/></td></tr>';
-    }
+    if (is_file(GW_UPLOADPATH . $row['screenshot']) && filesize(GW_UPLOADPATH . $row['screenshot']) > 0) {
+        echo '<td><img src="' . GW_UPLOADPATH . $row['screenshot'] . '" alt="Подтверждено" /></td></tr>';
+      }
+      else {
+        echo '<td><img src="' . GW_UPLOADPATH . 'unverified.gif' . '" alt="Не подтверждено!" /></td></tr>';
+      }
+      $i++;
   }
   echo '</table>';
 
