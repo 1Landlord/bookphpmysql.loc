e7c4796b4a8506ee26eaf63907342e8e96259e12
1Landlord <vl_che@bk.ru>
2018-01-18 06:33:28 +0300

    Создание страницы админка и сценария подтверждения и удаления данных из базы

---
+++ guitarwars/admin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Adminca</title>
+</head>
+<body>
+    <p><a href="index.php">К рейтингу</a></p>
+    <?php
+        //Инициализация констант
+        require_once('appvars.php');
+        //Cоединения с базой данных
+        require_once('connectvars.php');
+
+        //запрос извлечения данных, в порядке убывания score (score DESC)
+        $query = "SELECT * FROM guitarwars ORDER BY score DESC, date ASC";
+        $data = mysqli_query($dbc, $query);
+
+        // Извлечение данных из массива в цикле. Формирование данных записей в виде кода HTML 
+        echo '<table>';
+        while ($row = mysqli_fetch_array($data)) { 
+            // Вывод данных рейтинга
+            echo '<tr class="scorerow"><td><strong>' . $row['name'] . '</strong></td>';
+            echo '<td>' . $row['date'] . '</td>';
+            echo '<td>' . $row['score'] . '</td>';
+            //GET запрос через url на удаление из таблицы с помощью removescore.php
+            echo '<td><a href="removescore.php?id=' . $row['id'] .
+            '&amp;date=' . $row['date'] .
+            '&amp;name=' . $row['name'] . 
+            '&amp;score=' . $row['score'] .
+            '&amp;screenshot=' . $row['screenshot'] . 
+            '">Удалить</a></td></tr>';
+        }
+        echo '</table>';
+
+        mysqli_close($dbc);
+    ?>
+</body>
+</html>
+
+
+
---
+++ guitarwars/removescore.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Удаление рейтинга</title>
+</head>
+<body>
+<h2>Удаление рейтинга</h2>
+
+<?php
+    //Инициализация констант
+    require_once('appvars.php');
+
+    if (isset($_GET['id']) && isset($_GET['date']) && isset($_GET['name']) && isset($_GET['score']) && isset($_GET['screenshot'])) {
+        // Извлечение данных из суперглобального массива GET
+        $id = $_GET['id'];
+        $date = $_GET['date'];
+        $name = $_GET['name'];
+        $score = $_GET['score'];
+        $screenshot = $_GET['screenshot'];
+    }
+    else if (isset($_POST['id']) && isset($_POST['name']) && isset($_POST['score'])) {
+        // Извлечение данных из суперглобального массива POST
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $score = $_POST['score'];
+    }
+    else {
+        echo '<p class="error">Извините, ни одного рейтинга не выбрано для удаления.</p>';
+    }
+
+    if (isset($_POST['submit'])) {
+        if ($_POST['confirm'] == 'Yes') {
+        // Удаление с сервера файла изображений подтверждающего рейтинг
+        @unlink(GW_UPLOADPATH . $screenshot);
+
+        //Cоединения с базой данных
+        require_once('connectvars.php');
+
+        // Удаление рейтинга из базы данных
+        $query = "DELETE FROM guitarwars WHERE id = $id LIMIT 1";
+        mysqli_query($dbc, $query);
+        mysqli_close($dbc);
+
+        // Вывод пользователю страницы подтверждения
+        echo '<p>Рейтинг со значением ' . $score . ' для пользователя ' . $name . ' был успешно удален.';
+        }
+        else {
+        echo '<p class="error">Рейтинг не удален.</p>';
+        }
+    }
+    else if (isset($id) && isset($name) && isset($date) && isset($score)) {
+        echo '<p>Вы уверены, что хотите удалить этот рейтинг?</p>';
+        echo '<p><strong>Имя: </strong>' . $name . '<br /><strong>Дата: </strong>' . $date .
+        '<br /><strong>Рейтинг: </strong>' . $score . '</p>';
+        echo '<form method="post" action="removescore.php">';
+        echo '<input type="radio" name="confirm" value="Yes" /> Да ';
+        echo '<input type="radio" name="confirm" value="No" checked="checked" /> Нет <br />';
+        echo '<input type="submit" value="Удалить" name="submit" />';
+        echo '<input type="hidden" name="id" value="' . $id . '" />';
+        echo '<input type="hidden" name="name" value="' . $name . '" />';
+        echo '<input type="hidden" name="score" value="' . $score . '" />';
+        echo '</form>';
+    }
+
+    echo '<p><a href="admin.php">&lt;&lt; Назад к списку рейтингов</a></p>';
+    ?>
+</body>
+</html>
